08c754116ec981664bc8c46d669733ede78b5df0
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2024-11-25 14:06:04 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                ja: ["Zen Dots", serif],
+                en: ["Zen Antique Soft", serif],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
