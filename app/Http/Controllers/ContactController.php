0a9c9ffd9e062a1c92f016e4dc85e116320ae13a
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2024-12-06 15:59:44 +0900

    何とかメソッド通るようになった

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,19 +18,17 @@ class ContactController extends Controller
     }
 
     // 確認画面の表示（POSTメソッド）
-    public function confirm(ContactFormRequest $request)
+    public function confirm(Request $request)
     {
-        // 入力されたデータをセッションに保存
-        // $request->session()->put('contactData', $request->all());
-
         // 確認ページを表示
+        $contact = $request->all();
         return Inertia::render('Contact/confirm', [
-            'data' => $request->all()
-        ]);    
+            'data' => $contact
+        ]);
     }
 
     // 送信処理
-    public function send(ContactFormRequest $request)
+    public function send(Request $request)
     {
         $contact = $request->all();
 
@@ -38,13 +36,13 @@ class ContactController extends Controller
         $fromAddress = env('MAIL_FROM_ADDRESS');
         Mail::to($fromAddress)->send(new ContactSendmail($contact));
     
-        // セッションのデータをクリア
-        $request->session()->regenerateToken();  // CSRFトークン再生成
-        Session::forget('contactData');
+        // セッションのデータを渡す
+        $request->session()->regenerateToken();
+        $request->session()->forget('contact_form_data');
     
-        // 完了画面を表示（送信後にGETメソッドで遷移）
+        // 完了画面を表示
         return Inertia::render('Contact/thanks', [
-            'data' => $contact // 送信したデータを渡す
-        ]); 
+            'data' => $contact
+        ]);
     }
 }
--- resources/js/Components/Contact/ContactConfirm.tsx
+++ resources/js/Components/Contact/ContactConfirm.tsx
@@ -4,78 +4,76 @@ import PrimaryButton from '../PrimaryButton';
 import SecondaryButton from '../SecondaryButton';
 
 interface ContactData {
-	inquiryType: string,
-	companyType: string,
-	companyName: string,
-	name: string,
-	email: string,
-	message: string
+  inquiryType: string;
+  companyType: string;
+  companyName: string;
+  name: string;
+  email: string;
+  message: string;
 }
 
-export default function ConfirmContents({ data }: { data: any }) {
-	const [contact, setContact] = useState<ContactData>({
-		inquiryType: '',
-		companyType: '',
-		companyName: '',
-		name: '',
-		email: '',
-		message: ''
-	});
-	const metaCsrfToken = document.querySelector("meta[name='csrf-token']") as HTMLMetaElement;
-	const csrfToken = useRef<string>(metaCsrfToken.content);
+export default function ConfirmContents({ data }: { data: ContactData }) {
+  const [contact, setContact] = useState<ContactData>({
+    inquiryType: '',
+    companyType: '',
+    companyName: '',
+    name: '',
+    email: '',
+    message: ''
+  });
+  const metaCsrfToken = document.querySelector("meta[name='csrf-token']") as HTMLMetaElement;
+  const csrfToken = useRef<string>(metaCsrfToken.content);
 
-	useEffect(() => {
-		if (data) {
-		  setContact(data); // 親から受け取ったdataを状態にセット
-		}
-	  }, [data]);
-	  console.log({contact});
+  useEffect(() => {
+    if (data) {
+      setContact(data); // 親から受け取ったdataを状態にセット
+    }
+  }, [data]);
 
-	return (
-		<>
-			<Title title="contact" />
-			<div className="space-y-6 p-6 w-auto max-w-5xl mx-auto">
-				<form action="/contact/thanks" method="POST">
-					<input type="hidden" name="_token" value={csrfToken.current} />
-					<div className="grid grid-cols-1 gap-5 w-full h-auto">
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">お問い合わせ種類</p>
-							<p className="text-lg">{contact.inquiryType}</p>
-						</div>
-
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">個人・法人</p>
-							<p className="text-lg">{contact.companyType}</p>
-						</div>
-
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">会社名</p>
-							<p className="text-lg">{contact.companyName}</p>
-						</div>
-
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">お名前</p>
-							<p className="text-lg">{contact.name}</p>
-						</div>
-
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">メールアドレス</p>
-							<p className="text-lg">{contact.email}</p>
-						</div>
-
-						<div>
-							<p className="block mb-1 font-medium text-md text-slate-500 focus:outline-none">問い合わせ内容</p>
-							<p className="text-lg">{contact.message}</p>
-						</div>
-
-						<div className="flex justify-between gap-2">
-							<SecondaryButton type="button" onClick={() => window.history.back()}>Go to back</SecondaryButton>
-							<PrimaryButton type="submit">submit</PrimaryButton>
-						</div>
-					</div>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Title title="Contact" />
+      <div className="space-y-6 p-6 w-auto max-w-5xl mx-auto">
+        <form action="/contact/thanks" method="POST">
+          <input type="hidden" name="_token" value={csrfToken.current} />
+          <input type="hidden" name="inquiryType" value={contact.inquiryType} />
+          <input type="hidden" name="companyType" value={contact.companyType} />
+          <input type="hidden" name="companyName" value={contact.companyName} />
+          <input type="hidden" name="name" value={contact.name} />
+          <input type="hidden" name="email" value={contact.email} />
+          <input type="hidden" name="message" value={contact.message} />
+          <div className="grid grid-cols-1 gap-5 w-full h-auto">
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">お問い合わせ種類</p>
+              <p className="text-lg">{contact.inquiryType}</p>
+            </div>
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">個人・法人</p>
+              <p className="text-lg">{contact.companyType}</p>
+            </div>
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">会社名</p>
+              <p className="text-lg">{contact.companyName}</p>
+            </div>
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">お名前</p>
+              <p className="text-lg">{contact.name}</p>
+            </div>
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">メールアドレス</p>
+              <p className="text-lg">{contact.email}</p>
+            </div>
+            <div>
+              <p className="block mb-1 font-medium text-md text-slate-500">問い合わせ内容</p>
+              <p className="text-lg">{contact.message}</p>
+            </div>
+            <div className="flex justify-between gap-2">
+              <SecondaryButton type="button" onClick={() => window.history.back()}>Go to back</SecondaryButton>
+              <PrimaryButton type="submit">Submit</PrimaryButton>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
-
