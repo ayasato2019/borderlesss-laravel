30a4849c841ff0d4480c4fae690eb17004ffda74
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2024-12-12 09:54:08 +0900

    headerに正しいURL追記

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- canonical -->
+    <link rel="canonical" href="{{ url()->current() }}" />
     <!-- CSRF -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- トラッキング -->
